6cc7e892003fdb5fe856c07b4961dba2daf28df9
Diego Samudio <dieguisam@gmail.com>
2023-06-02 17:26:26 -0500

    validacion de carpetas y ramas

--- src/Routes/AppRoutes.jsx
+++ src/Routes/AppRoutes.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import Login from "../pages/Login/Login";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Home from "../pages/Home/Home";
-
+import Search from "../pages/Search/Search";
 import Detail from "../pages/Detail/Detail";
 import ShoppingCart from "../pages/ShoppingCart/ShoppingCart";
 import OrderSuccessful from "../pages/OrderSuccessful/OrderSuccessful";
@@ -16,6 +16,8 @@ const AppRoutes = () => {
         <Route path="/">
         <Route index element={<Login />} />
             <Route path="Home" element={<Home />}>
+            </Route>
+            <Route path="Search" element={<Search />}>
             </Route>
             <Route path="Detail" element={<Detail />}>
             </Route>
---
+++ src/pages/Search/Search.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+function Search() {
+  return (
+    <div>
+      <h1>busquemos alg / se usa use context / formik , no hacer un hook de formulario sino usar formik</h1>
+        <h1>validacions YUP</h1>
+    </div>
+  )
+}
+
+export default Search
